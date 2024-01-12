2032b7ac2aa4cfee63ee5f24d3ad2946544b43d2
anonpran <nandapranesh27@gmail.com>
2024-01-12 16:41:05 +0530

    Fourth commit

--- milestone2.py
+++ milestone2.py
@@ -1,28 +1,66 @@
+def milestone2(diameter,size,shift,dow):
+
+    #Step 1:
+    #given the centre(0,0) now will have to move towards that cell
+    #shfit vector is pointing towards
+    x, y = 0,0
+    try:
+        x_new = x + x_new
+        y_new = y + y_new
+
+    except IOError:
+        print("Error moving")
+
+
+    #Step 2:
+    #Fixing the reference point given as index (0,0)
+    try:
+
+
+    except Exception as e:
+        print("Indexing error",e)
+
+
+    #Step 3: 
+    #Implementing a dfs search algorithm to search for all valid cells
+    def dfs(i,j,r,c,diameter):
+        #to hold the locations that has already been visisted
+        seen=[]
+        r = diameter /2
+        c = diameter / 2
+        #rx and ry are reference coordinates and checking all boundary conditions
+        if i > r or j > c or i < (-diameter) or j < (-diameter):
+            return 0
+        
+        #to remove recursive calling problem
+        seen.append([i,j])
+        #else keep calling a dfs method to move in all possible directions
+        #horizontal and vertical
+        dfs(i-1,j,r,c,diameter)
+        dfs(i+1,j,r,c,diameter)
+        dfs(i,j-1,r,c,diameter)
+        dfs(i,j+1,r,c,diameter)
+
 #Opening the file
 try:
     arr=[]
     filename = input()
     f = open(filename,'r')
-    #print(f.read())
-    #print(f.readline())
+    arr.append(f.readline().split(':'))
     arr.append(f.readline().split(':'))
     arr.append(f.readline().split(':'))
     arr.append(f.readline().split(':'))
     inp = []
     for i in range(len(arr)):
         inp.append(int(arr[i][1].strip()))
-    #print(inp)
 
     wafer_diameter = inp[0]
-    number = inp[1]
-    angle=inp[2]
-    #print(wafer_diameter)
-    #print(number)
-    #print(angle)
+    size = inp[1]
+    shift=inp[2]
+    dow = inp[3]
 
-    milestone1(wafer_diameter,number,angle)
+    milestone2(wafer_diameter,size,shift,dow)
 
 
 except IOError:
     print("Error taking the input")
-#print(arr[0])
--- task1.py
+++ task1.py
@@ -1,33 +1,34 @@
 import math
 import numpy as np
-#import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as R
 
 
 def milestone1(d,n,a):
+    try:
+        radius = d/2
+        # Calculate the radius based on the given diameter
+        radius = d / 2
 
-    radius = d/2
-    # Calculate the radius based on the given diameter
-    radius = d / 2
+        # Generate equidistant points on the x-axis
+        num_points = n
+        theta_degrees = a
+        x_points = np.linspace(-radius, radius, num_points)
 
-    # Generate equidistant points on the x-axis
-    num_points = n
-    theta_degrees = a
-    x_points = np.linspace(-radius, radius, num_points)
+        # Rotate points by the given angle (theta) in degrees
+        theta_radians = np.radians(theta_degrees)
+        x_rotated = x_points * np.cos(theta_radians) - np.zeros_like(x_points) * np.sin(theta_radians)
+        y_rotated = x_points * np.sin(theta_radians) + np.zeros_like(x_points) * np.cos(theta_radians)
+        
+        #print(x_rotated, y_rotated)
 
-    # Rotate points by the given angle (theta) in degrees
-    theta_radians = np.radians(theta_degrees)
-    x_rotated = x_points * np.cos(theta_radians) - np.zeros_like(x_points) * np.sin(theta_radians)
-    y_rotated = x_points * np.sin(theta_radians) + np.zeros_like(x_points) * np.cos(theta_radians)
-    
-    #print(x_rotated, y_rotated)
-
-    with open("21pc19_4.txt","w") as f:
-        for i in range(len(x_rotated)):
-            tempx =str(round(x_rotated[i],4)).strip('[')
-            tempy = str(str(round(y_rotated[i],4)).strip(']'))
-            f.write("("+tempx+","+tempy+")")
-            f.write("\n")
+        with open("21pc19_4.txt","w") as f:
+            for i in range(len(x_rotated)):
+                tempx =str(round(x_rotated[i],4)).strip('[')
+                tempy = str(str(round(y_rotated[i],4)).strip(']'))
+                f.write("("+tempx+","+tempy+")")
+                f.write("\n")
+    except Exception as e:
+        print("The error is: ",e)
 
 #Opening the file
 try:
